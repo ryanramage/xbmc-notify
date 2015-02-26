c4bc7588563623fcba6c9b0590a91bd990a45aa4
Ryan Ramage <ryan.ramage@gmail.com>
2015-02-25 19:00:42 -0700

    add play and better cli support

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 var url = require('url')
 		, request = require('request')
-		
-module.exports = function(host, title, message, cb) {
+
+exports.notify = function(host, title, message, cb) {
 	var	qs = {
 			jsonrpc:"2.0",
 			method:"GUI.ShowNotification",
 			params:{
 				title: title,
 				message: message
+			},"id":1
+		}
+  var dets = JSON.stringify(qs).replace(/"/g, "%22")
+  var endpoint = url.resolve(host, '/jsonrpc?request=' + dets);
+	request(endpoint, cb)
+
+}
+
+
+exports.play = function(host, api, cb) {
+	var	qs = {
+			jsonrpc:"2.0",
+			method:"Player.Open",
+			params:{
+				item: {
+					file: api
+				}
 			},"id":1
 		}
   var dets = JSON.stringify(qs).replace(/"/g, "%22")
